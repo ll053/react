7fceee844d17f413f93b188c4f8a557610cf3dcc
LeahLesser <154979144+LeahLesser@users.noreply.github.com>
2024-06-24 21:18:45 +0300

    first

---
+++ src/App.js
@@ -0,0 +1,25 @@
+
+
+import './App.css';
+
+import React from 'react';
+
+
+const Enter = () => {
+  return (
+    <div>
+      <header>
+        <h1>recovery home</h1>
+        <p>Welcome to the luxurious Bricket Hotel. Your home away from home.</p>
+      </header>
+    </div>
+  );
+}
+
+export default Enter;
+
+
+// App.js
+
+
+
